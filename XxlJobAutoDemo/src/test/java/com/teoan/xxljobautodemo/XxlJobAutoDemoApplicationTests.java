d8cdd657da78dec590abe525d47cd77f7cd6c74f
Teoan <d277077411@gmail.com>
2023-05-22 20:56:49 +0800

    新增XxlJobAuto相关Demo代码

---
+++ XxlJobAutoDemo/src/test/java/com/teoan/xxljobautodemo/XxlJobAutoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teoan.xxljobautodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxlJobAutoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
